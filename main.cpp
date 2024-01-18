1b30984b4a1595d09ebd6aa6a85d58d64bcf5e01
LiSA <m11217028@gemail.yuntech.edu.tw>
2024-01-18 18:23:13 +0800

    level one init version2

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include <string>
 
 using namespace std;
-class Utility_chain_prefix_node {
+class Utility_chain_prefix_node{
 public:
     int tid_prefix;
     int acu;
@@ -18,7 +18,7 @@ public:
         next = nullptr;
     }
 };
-class Utility_chain_sid_node {
+class Utility_chain_sid_node{
 public:
     int sid;
     int peu_sid;
@@ -48,7 +48,7 @@ public:
     }
 };
 
-void init_UMatrix_RMatrix(vector<vector<vector<int>>> &Utility_Matrix_set,vector<vector<vector<int>>> &Utility_Ru_Matrix_set,int &item_quantity){
+void init_UMatrix_RMatrix(vector<vector<vector<int>>> &Utility_Matrix_set,vector<vector<vector<int>>> &Utility_Ru_Matrix_set,int &item_quantity,map<int,int> &single_item_iu,map<int,int> &single_item_swu){
     ifstream in;
     in.open("uspanutb.txt");
     if(in.fail()){
@@ -61,22 +61,31 @@ void init_UMatrix_RMatrix(vector<vector<vector<int>>> &Utility_Matrix_set,vector
         in >> item >> utility;
         item--;
         Utility_table[item]=utility;
+        single_item_iu[item]=0;
+        single_item_swu[item]=0;
     }
     if(Utility_table.empty()){
         exit(1);
     }
     in.close();
 
-
     in.open("uspandata.txt");
+    if(in.fail()){
+        cout << "input file opening failed";
+        exit(1);
+    }
 
+    item_quantity = Utility_table.size();
+
+    vector<bool> bitmap_item_exist(item_quantity,false);//這要拿來做SWU和IU
+    vector<int> max_iu_item(item_quantity,0);
 
     vector<vector<int>> Utility_Matrix;
     vector<vector<int>> Utility_Ru_Matrix;
 
     vector<int> itemset_ru;
 
-    item_quantity = Utility_table.size();
+
     int sequence_total_utility=0;
     vector<int> Itemset(item_quantity,0);
     int sid,itemset_id,quantity;
@@ -85,11 +94,19 @@ void init_UMatrix_RMatrix(vector<vector<vector<int>>> &Utility_Matrix_set,vector
     while(!in.eof()) {
         in >> sid >> itemset_id >> item >> quantity;
         item--;
-        if(run_times>0 && pre_itemset_id!=itemset_id){
+
+        if(run_times>0 && pre_sid!=sid){
             Utility_Matrix.push_back(Itemset);
             fill(Itemset.begin(),Itemset.end(),0);
-        }
-        if(run_times>0 && pre_sid!=sid){
+
+            for(int i=0;i<item_quantity;i++){
+                if(bitmap_item_exist[i]){
+                    single_item_swu[i]+=sequence_total_utility;
+                    single_item_iu[i]+=max_iu_item[i];
+                }
+            }
+            fill(bitmap_item_exist.begin(),bitmap_item_exist.end(),0);
+            fill(max_iu_item.begin(),max_iu_item.end(),0);
 
             for(int i=0;i<Utility_Matrix.size();i++){
                 for(int j=0;j<Utility_Matrix[i].size();j++){
@@ -106,16 +123,37 @@ void init_UMatrix_RMatrix(vector<vector<vector<int>>> &Utility_Matrix_set,vector
             Utility_Ru_Matrix.clear();
             sequence_total_utility = 0 ;
 
+        }else if(run_times>0 && pre_itemset_id!=itemset_id){
+            Utility_Matrix.push_back(Itemset);
+            fill(Itemset.begin(),Itemset.end(),0);
+
         }
+
+        //cout<<item;
+        bitmap_item_exist[item]=true;
+        if(max_iu_item[item]<Utility_table[item] * quantity){
+            max_iu_item[item]=Utility_table[item] * quantity;
+        }
+
         pre_sid = sid;
         pre_itemset_id = itemset_id;
         run_times++;
-
         Itemset[item] = Utility_table[item] * quantity;
         sequence_total_utility += Utility_table[item] * quantity;
+
     }
     if(run_times>0){ // 最後一個sid要加進matrix
         Utility_Matrix.push_back(Itemset);
+        fill(Itemset.begin(),Itemset.end(),0);
+
+        for(int i=0;i<item_quantity;i++){
+            if(bitmap_item_exist[i]){
+                single_item_swu[i]+=sequence_total_utility;
+                single_item_iu[i]+=max_iu_item[i];
+            }
+        }
+        fill(bitmap_item_exist.begin(),bitmap_item_exist.end(),0);
+        fill(max_iu_item.begin(),max_iu_item.end(),0);
 
         for(int i=0;i<Utility_Matrix.size();i++){
             for(int j=0;j<Utility_Matrix[i].size();j++){
@@ -135,11 +173,15 @@ void init_UMatrix_RMatrix(vector<vector<vector<int>>> &Utility_Matrix_set,vector
     in.close();
 
 }
-void init_level1(const vector<vector<vector<int>>> &Utility_Matrix_set,const vector<vector<vector<int>>> &Utility_Ru_Matrix_set,const int &item_quantity){
+void init_level1(const vector<vector<vector<int>>> &Utility_Matrix_set,const vector<vector<vector<int>>> &Utility_Ru_Matrix_set,const int &item_quantity,const int &threshold,const map<int,int> &single_item_swu,map<int,Seq_table*> &single_item_Utility_Chain){
     Seq_table * Seq_Table;
     int sid_peu,iu_add_ru,iu;
     int Pue,Iu;
-    for(int item=0;item<item_quantity;item++){//test
+    for(int item=0;item<item_quantity;item++){
+        //cout<<single_item_swu.at(item)<<"\n\n";
+        if(single_item_swu.at(item)<threshold){
+            continue;
+        }
         Seq_Table = new Seq_table;
         Seq_Table->seq += to_string(item);
         Seq_Table->sid_node_head = new Utility_chain_sid_node;
@@ -195,20 +237,28 @@ void init_level1(const vector<vector<vector<int>>> &Utility_Matrix_set,const vec
             }
             Seq_Table->Peu = Pue;
             Seq_Table->U_t = Iu;
-            cout<<Seq_Table->Peu<<"\n";
-            cout<<Seq_Table->U_t<<"\n\n";
+//            cout<<Seq_Table->seq<<"\n";
+//            cout<<Seq_Table->Peu<<"\n";
+//            cout<<Seq_Table->U_t<<"\n\n";
         }
+        single_item_Utility_Chain[item]=Seq_Table;
+        //delete Seq_Table;
 
     }
 }
 
+
 int main() {
-    int item_quantity;//item-> a=0,b=1,...
+    int item_quantity,threshold=100;//item-> a=0,b=1,...
     vector<vector<vector<int>>> Utility_Matrix_set;
     vector<vector<vector<int>>> Utility_Ru_Matrix_set;
-    init_UMatrix_RMatrix(Utility_Matrix_set,Utility_Ru_Matrix_set,item_quantity);
-    init_level1(Utility_Matrix_set,Utility_Ru_Matrix_set,item_quantity);
+    map<int,int> single_item_iu,single_item_swu;
+    init_UMatrix_RMatrix(Utility_Matrix_set,Utility_Ru_Matrix_set,item_quantity,single_item_iu,single_item_swu);
+    map<int,Seq_table*> single_item_Utility_Chain;
+    init_level1(Utility_Matrix_set,Utility_Ru_Matrix_set,item_quantity,threshold,single_item_swu,single_item_Utility_Chain);
 
+
+    //cout<<single_item_iu[5]<<" "<<single_item_swu[5];
     //cout<<item_quantity;
 //    for(int i=0;i<Utility_Matrix_set.size();i++){
 //        for(int j=0;j<Utility_Matrix_set[i].size();j++){
